f0a36588dfa1bdb2ec4fc926a579c224adc62876
Jahsie Ault <jahsieault@gmail.com>
2015-04-14 14:14:50 -0700

    First step to convert site to new design. Changes to order of HTML, css styles and minor changes to google maps JS.

--- js/main.js
+++ js/main.js
@@ -1,314 +1,3 @@
-console.log('butt!!')
-// var styles = 
-// [
-//     {
-//         "featureType": "administrative",
-//         "elementType": "labels.text.fill",
-//         "stylers": [
-//             {
-//                 "color": "#444444"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "landscape",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "color": "#f2f2f2"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "visibility": "on"
-//             },
-//             {
-//                 "hue": "#ffb200"
-//             },
-//             {
-//                 "saturation": "100"
-//             },
-//             {
-//                 "lightness": "-21"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi",
-//         "elementType": "labels.icon",
-//         "stylers": [
-//             {
-//                 "visibility": "on"
-//             },
-//             {
-//                 "weight": "2.67"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi.attraction",
-//         "elementType": "geometry.stroke",
-//         "stylers": [
-//             {
-//                 "visibility": "on"
-//             },
-//             {
-//                 "color": "#ffb200"
-//             },
-//             {
-//                 "weight": "4.86"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi.attraction",
-//         "elementType": "labels.text.stroke",
-//         "stylers": [
-//             {
-//                 "hue": "#ff7900"
-//             },
-//             {
-//                 "weight": "1.35"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi.attraction",
-//         "elementType": "labels.icon",
-//         "stylers": [
-//             {
-//                 "saturation": "0"
-//             },
-//             {
-//                 "color": "#fbff00"
-//             },
-//             {
-//                 "weight": "2.93"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "saturation": -100
-//             },
-//             {
-//                 "lightness": 45
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.highway",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "visibility": "simplified"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.arterial",
-//         "elementType": "labels.icon",
-//         "stylers": [
-//             {
-//                 "visibility": "off"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "transit",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "visibility": "off"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "water",
-//         "elementType": "all",
-//         "stylers": [
-//             {
-//                 "color": "#097faf"
-//             },
-//             {
-//                 "visibility": "on"
-//             }
-//         ]
-//     }
-// ]
-
-// // var styles = [
-// //     {
-// //         "featureType": "administrative",
-// //         "elementType": "labels.text.fill",
-// //         "stylers": [
-// //             {
-// //                 "color": "#444444"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "landscape",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "color": "#f2f2f2"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "poi",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "visibility": "off"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "road",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "saturation": -100
-// //             },
-// //             {
-// //                 "lightness": 45
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "road.highway",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "visibility": "simplified"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "road.arterial",
-// //         "elementType": "labels.icon",
-// //         "stylers": [
-// //             {
-// //                 "visibility": "off"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "transit",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "visibility": "off"
-// //             }
-// //         ]
-// //     },
-// //     {
-// //         "featureType": "water",
-// //         "elementType": "all",
-// //         "stylers": [
-// //             {
-// //                 "color": "#46bcec"
-// //             },
-// //             {
-// //                 "visibility": "on"
-// //             }
-// //         ]
-// //     }
-// // ];
-
-// var myCenter=new google.maps.LatLng(45.5136190,-122.6520430);
-
-// var myCenter2=new google.maps.LatLng(45.517555, -122.631819);
-
-// function initialize() {
-// 	var mapProp = {
-// 		center:myCenter,
-// 		zoom:14,
-// 		styles: styles
-// 	};
-
-// 	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-// 	var marker=new google.maps.Marker({
-// 		position:myCenter,
-// 		icon:'jollyRog.png',
-// 		info: contentString,
-// 		labelClass: 'mapMarker',
-// 		// size: new google.maps.Size(20, 32)
-// 	});
-
-// 	var marker2=new google.maps.Marker({
-// 		position:myCenter2,
-// 		icon:'fontina.png',
-// 		info: contentString2,
-// 		strokeColor: '#FF0000',
-// 		strokeWeight: '10px'
-// 		// size: new google.maps.Size(20, 32)
-// 	});
-
-// 	marker.setMap(map);
-// 	marker2.setMap(map);
-
-//   	var contentString = '<div id="content">'+
-//     	'<div id="siteNotice">'+
-//     	'</div>'+
-//     	'<h1 id="firstHeading" class="firstHeading">12th and Madison!</h1>'+
-//     	'<div id="bodyContent">'+
-//     	'<p><b>Mural</b>, Blah blah blah, creepy mural next to the Jolly Roger</p>'+
-//     	'<p>#mural #graffiti #art</p>'+
-//     	'</div>'+
-//     	'</div>';
-
-//     var contentString2 = '<div id="content">'+
-//     	'<div id="siteNotice">'+
-//     	'</div>'+
-//     	'<h1 id="firstHeading" class="firstHeading">33rd and Morrison</h1>'+
-//     	'<div id="bodyContent">'+
-//     	'<p><b>Mural</b>, Blah blah blah, cool mural next to the Jolly Roger</p>'+
-//     	'<p>#mural #graffiti #art</p>'+
-//     	'</div>'+
-//     	'</div>';
-
-//     var infowindow = new google.maps.InfoWindow({
-//     	content: contentString
-//   	});
-
-//   	var infowindow2 = new google.maps.InfoWindow({
-//     	content: contentString2
-//   	});
-
-// 	google.maps.event.addListener(marker, 'mouseover', function() {
-//     	infowindow.open(map,marker);
-//     });
-
-// 	google.maps.event.addListener(marker, 'mouseout', function() {
-//     	infowindow.close(map,marker);
-// 	});
-
-
-// 	google.maps.event.addListener(marker2, 'mouseover', function() {
-//     	infowindow2.open(map,marker2);
-//     });
-
-// 	google.maps.event.addListener(marker2, 'mouseout', function() {
-//     	infowindow2.close(map,marker2);
-// 	});
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
 google.maps.event.addDomListener(window, 'load', init);
 var map;
 
@@ -348,8 +37,10 @@ function init() {
     var mapElement = document.getElementById('google-map');
     var map = new google.maps.Map(mapElement, mapOptions);
     var locations = [
-		['Spot 1', 'This is spot 1. Blah blah. #this #that', 'undefined', 'undefined', 'undefined', 45.5136190, -122.6520430, 'img/fontina.png'],
-		['Spot 2', 'This is spot 2. Beep boop. #beep #boop', 'undefined', 'undefined', 'undefined', 45.517555,  -122.631819, 'img/jollyRog.png']
+		['Spot 1', 'This is spot 1. Blah blah. #this #that', 'undefined', 'undefined', 'undefined', 45.5136190, -122.6520430, 'img/map-marker-image.png'],
+		['Spot 2', 'This is spot 2. Beep boop. #beep #boop', 'undefined', 'undefined', 'undefined', 45.5196190, -122.6590430, 'img/map-marker-selected.png'],
+    ['Spot 3', 'This is spot 3. Blah blah. #this #that', 'undefined', 'undefined', 'undefined', 45.5316190, -122.6320430, 'img/map-marker-image.png'],
+    ['Spot 4', 'This is spot 4. Beep boop. #beep #boop', 'undefined', 'undefined', 'undefined', 45.517555,  -122.631819, 'img/map-marker-image.png']
     ];
     for (i = 0; i < locations.length; i++) {
 		if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
@@ -368,39 +59,44 @@ function init() {
             // web: web,
             id: 'markerLayer'
         });
-      link = '';            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+        
+      // ADDS INFO WINDOW TO MARKER  
+      // link = '';            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
       var myoverlay = new google.maps.OverlayView();
      	myoverlay.draw = function () {
         this.getPanes().markerLayer.id='markerLayer';
      };
      myoverlay.setMap(map);
  }
-function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
-  var infoWindowVisible = (function () {
-          var currentlyVisible = false;
-          return function (visible) {
-              if (visible !== undefined) {
-                  currentlyVisible = visible;
-              }
-              return currentlyVisible;
-           };
-       }());
-       iw = new google.maps.InfoWindow({maxWidth: 100});
-       google.maps.event.addListener(marker, 'click', function() {
-           if (infoWindowVisible()) {
-               iw.close();
-               infoWindowVisible(false);
-           } else {
-               var html= "<div style='color:#000;background-color: white;padding:5px;' class='infoWindow'><h4>"+title+"</h4><p>"+desc+"<p></div>";
-               iw = new google.maps.InfoWindow({content:html});
-               iw.open(map,marker);
-               infoWindowVisible(true);
-           }
-    });
-    google.maps.event.addListener(iw, 'closeclick', function () {
-        infoWindowVisible(false);
-    });
-}
+
+ ///////////INFO WINDOW CODE - might recycle for sidebar div /////////////
+
+// function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+//   var infoWindowVisible = (function () {
+//           var currentlyVisible = false;
+//           return function (visible) {
+//               if (visible !== undefined) {
+//                   currentlyVisible = visible;
+//               }
+//               return currentlyVisible;
+//            };
+//        }());
+//        iw = new google.maps.InfoWindow({maxWidth: 100});
+//        google.maps.event.addListener(marker, 'click', function() {
+//            if (infoWindowVisible()) {
+//                iw.close();
+//                infoWindowVisible(false);
+//            } else {
+//                var html= "<div style='color:#000;background-color: white;padding:5px;' class='infoWindow'><h4>"+title+"</h4><p>"+desc+"<p></div>";
+//                iw = new google.maps.InfoWindow({content:html});
+//                iw.open(map,marker);
+//                infoWindowVisible(true);
+//            }
+//     });
+//     google.maps.event.addListener(iw, 'closeclick', function () {
+//         infoWindowVisible(false);
+//     });
+// }
 }
 
 console.log('butt')
