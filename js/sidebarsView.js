9bfb1574d0e4210c0e07ea2484c4a02ccece9066
Jahsie Ault <jahsieault@gmail.com>
2015-04-16 21:46:17 -0700

    Updates to responsive design. Bug in window resizing in chrome, works fine in Fireforx. Working on a fix

--- js/sidebarsView.js
+++ js/sidebarsView.js
@@ -10,12 +10,13 @@ var sidebars = Backbone.View.extend({
 	initialize: function(){
 		oddity = "<div id=\"guide-button-div\" class=\"info-dropdown center\">\
 			<h2>Closest Curiosties</h2>\
-			<p>\
-				HI HI HI Magnis eros enim cursus. Et cursus adipiscing mauris ut aenean scelerisque tortor, facilisis adipiscing placerat scelerisque auctor platea, arcu in habitasse nunc pellentesque! Ut auctor et ut vel. In magna aliquet non! Ultrices sit sit nunc, enim turpis placerat ultricies dis lacus? Aliquet nec et in integer in ac elit placerat sed magna sagittis scelerisque!\
-			</p>\
-			<p>\
-				Sed! Urna! Egestas turpis hac enim nascetur arcu integer, risus augue sit ultrices facilisis egestas, massa amet, porttitor montes nisi, pulvinar est vut porttitor enim? Tincidunt? Mus, sed platea a etiam eu aliquam ut enim. Etiam risus porttitor adipiscing velit mus, urna dapibus, nec! Natoque placerat, urna, non in magna, nisi elit odio! Vel risus nunc urna mauris urna facilisis et urna facilisis rhoncus integer.\
-			</p>\
+			<div class='curiousProfile'>\
+			<h3>Angst on Angst</h3>\
+			<img class='curious-img' src='./img/jollyRog.jpg'>\
+			<p class='curious-description'>This is a weird and ugly mural of some sort of hipster meta-angst.  #ugly</p>\
+			</div>\
+			<p class='curious-type'>Category:<br>\
+			Art</p>\
 			</div>";
 
 		about = "<div id=\"about-button-div\" class=\"info-dropdown center\">\
